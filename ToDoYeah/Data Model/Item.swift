06ea71596a113f7b16a3acae0764b3f8617acfa2
Anais Bussiere <anais.bussiere@gmail.com>
2018-03-10 18:05:15 +0100

    Convert to MVC design pattern

---
+++ ToDoYeah/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoYeah
+//
+//  Created by Anais Bussiere on 10/03/2018.
+//  Copyright © 2018 BKT Labs. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
